02438da14b6711845ffbd398ac05a4d3c7d1cc9e
Thiago Aires da Silva <makarrao@gmail.com>
2022-06-29 21:33:00 -0300

    backend: first server (AKA hello server?)

--- backend/bin/backend.dart
+++ backend/bin/backend.dart
@@ -1,5 +1,10 @@
 import 'package:backend/backend.dart' as backend;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
-void main(List<String> arguments) {
-  print('Hello world: ${backend.calculate()}!');
+void main(List<String> args) async {
+  final server = await shelf_io.serve(
+      (req) => Response(200, body: 'ok'), 'localhost', 8081);
+
+  print("Server started @ http://localhost:8081");
 }
